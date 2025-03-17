601aac2df6e1d8be463f403a3e98232ad690adc8
urichh <urban.dopudja@gmail.com>
2025-03-18 00:32:43 +0100

    mathjax fixed for ssr

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { CollapsibleSectionComponent } from '../collapsible-section/collapsible-section.component';
 import { CollapsibleSubsectionComponent } from '../collapsible-subsection/collapsible-subsection.component';
-//import { MathjaxDirective } from '../mathjax.directive';
+import { MathjaxDirective } from '../mathjax.directive';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CollapsibleSectionComponent, CollapsibleSubsectionComponent, /*MathjaxDirective*/],
+  imports: [CollapsibleSectionComponent, CollapsibleSubsectionComponent, MathjaxDirective],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
--- src/app/mathjax.directive.ts
+++ src/app/mathjax.directive.ts
@@ -1,19 +1,52 @@
-/*import { Directive, ElementRef, Input, AfterViewInit } from '@angular/core';
+import { Directive, ElementRef, Input, AfterViewInit, PLATFORM_ID, Inject } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 
 @Directive({
   selector: '[appMathjax]'
 })
 export class MathjaxDirective implements AfterViewInit {
-  @Input() appMathjax?: string;
+  @Input('appMathjax') mathExpression: string = '';
 
-  constructor(private el: ElementRef) {}
+  constructor(
+    private el: ElementRef,
+    @Inject(PLATFORM_ID) private platformId: object
+  ) {}
 
-  ngAfterViewInit(): void {
-    this.renderMath();
+  ngAfterViewInit() {
+    if (isPlatformBrowser(this.platformId)) {
+      this.loadMathJax();
+    }
   }
 
-  private renderMath(): void {
-    this.el.nativeElement.innerHTML = this.appMathjax;
-    (window as any).MathJax.typesetPromise();
+  private loadMathJax() {
+    if (typeof window !== 'undefined' && (window as any).MathJax) {
+      this.el.nativeElement.innerHTML = this.mathExpression;
+      setTimeout(() => { //ssr problemi (caching?)
+        (window as any).MathJax.typesetClear();
+        (window as any).MathJax.typesetPromise()
+        .then(() => console.log("MathJax rendering complete"))
+        .catch((err: any) => console.error('MathJax render error:', err));
+    }, 50);
+    } else {
+      this.loadMathJaxScript();
+    }
   }
-}*/
+
+  private loadMathJaxScript() {
+    if ((window as any).MathJax) {
+        console.log("MathJax already loaded, reprocessing...");
+        this.loadMathJax(); //procesiraj sam xpr?
+        return;
+    }
+    
+    const script = document.createElement('script');
+    script.id = 'mathjax-script';
+    script.type = 'text/javascript';
+    script.src = 'https://cdnjs.cloudflare.com/ajax/libs/mathjax/3.2.2/es5/tex-mml-chtml.js';
+    script.async = true;
+    script.onload = () => {
+    this.loadMathJax(); //fix za problemi z renderajnem dynamic mathjax na clientu pr ssr - nope ig
+    };
+    document.head.appendChild(script);
+  }
+}
